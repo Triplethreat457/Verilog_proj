822a849fa38afb41ad71294a30a128964887ecb8
Triplethreat457 <antmcdon@umich.edu>
2026-08-27 03:34:11 -0400

    made some changes to clk div + organization

--- SV/SPI_CONTROLLER/rtl/clock_dvider.sv
+++ SV/SPI_CONTROLLER/rtl/clock_dvider.sv
@@ -1,25 +1,30 @@
-module clock_divider #(parameter int CLK_DIV = 10)(
+module clock_divider #(parameter int CLK_DIV = 10)
+// CLK divsion how much do you want to make the period longer
+// Example 10 clk_period per one cycles so 5 cycles would be oscillation starts 
+
+(
+    
     input logic clk,
     input logic enable, //Signal to keep shifting and 
     input logic rst_n,
     output logic sclk_rising,
     output logic sclk_falling
 );
-
+int clk_cnt = 0;
 always_ff @(posedge clk) begin 
-   if(enable) begin
+   if (enable) begin
+    clk_cnt = clk_cnt + 1;
+    if((CLK_DIV/2 - 1) == clk_cnt) begin
+
+
+    end
+   end else begin 
+
 
    end
 
 
 end
-
-always_ff @(negedge clk) begin
-
-
-end
-
-
 
 
 
